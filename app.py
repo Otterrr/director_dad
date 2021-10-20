34f8eab87c8028064b08d8df08966ae72acc76be
Dan Otter <danco77@virginmedia.com>
2021-10-20 14:50:44 +0000

    Added User Profile page & connected

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -61,8 +62,10 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
-                # return redirect(url_for("profile", username=session["user"]))
+                flash("Welcome, {}".format(
+                            request.form.get("username")))
+                return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # invalid pw
                 flash("Incorrect Username and/or Password")
@@ -74,6 +77,18 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
