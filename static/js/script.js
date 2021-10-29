e489f19c1c6d3e091081d2333a2debd64a53ca76
Dan Otter <danco77@virginmedia.com>
2021-10-29 17:33:26 +0000

    Changed auto complete links from 8080 to Heroku to open in heroku deployment

--- static/js/script.js
+++ static/js/script.js
@@ -37,73 +37,73 @@ $(document).ready(function () {
       var value = $('input.autocomplete').val();
 
       if (value == "Dune") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/6177271fdc188d91fa0c1d2f', '_self');
+        open('http://director-dad.herokuapp.com/view_films/6177271fdc188d91fa0c1d2f', '_self');
 
       } else if (value == "Free Guy") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617726c4dc188d91fa0c1d2e', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617726c4dc188d91fa0c1d2e', '_self');
 
       } else if (value == "No Time to Die") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/616eacc871dc8003743d58d7', '_self');
+        open('http://director-dad.herokuapp.com/view_films/616eacc871dc8003743d58d7', '_self');
 
       } else if (value == "1917") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a994ddb4612c6a28685e2', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a994ddb4612c6a28685e2', '_self');
 
       } else if (value == "The Invisible Man") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9ce86a3c12db4ad1e798', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9ce86a3c12db4ad1e798', '_self');
 
       } else if (value == "The Lion King") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a994ddb4612c6a28685e2', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a994ddb4612c6a28685e2', '_self');
 
       } else if (value == "Bombshell") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9b316a3c12db4ad1e791', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9b316a3c12db4ad1e791', '_self');
 
       } else if (value == "Bad Boys for Life") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9b4c6a3c12db4ad1e792', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9b4c6a3c12db4ad1e792', '_self');
 
       } else if (value == "The Gentlemen") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9b6b6a3c12db4ad1e793', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9b6b6a3c12db4ad1e793', '_self');
 
       } else if (value == "The Rhythm Section") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9b8c6a3c12db4ad1e794', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9b8c6a3c12db4ad1e794', '_self');
 
       } else if (value == "Birds of Prey") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9c176a3c12db4ad1e795', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9c176a3c12db4ad1e795', '_self');
 
       } else if (value == "A Beautiful Day in the Neighborhood") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9c4f6a3c12db4ad1e796', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9c4f6a3c12db4ad1e796', '_self');
 
       } else if (value == "Parasite") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9ca26a3c12db4ad1e797', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9ca26a3c12db4ad1e797', '_self');
 
       } else if (value == "Downhill") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9d2b6a3c12db4ad1e799', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9d2b6a3c12db4ad1e799', '_self');
 
       } else if (value == "Emma.") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9d696a3c12db4ad1e79a', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9d696a3c12db4ad1e79a', '_self');
 
       } else if (value == "Black Widow") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9ded6a3c12db4ad1e79b', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9ded6a3c12db4ad1e79b', '_self');
 
       } else if (value == "A Quiet Place Part II") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9e306a3c12db4ad1e79c', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9e306a3c12db4ad1e79c', '_self');
 
       } else if (value == "Cruella") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9e646a3c12db4ad1e79d', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9e646a3c12db4ad1e79d', '_self');
 
       } else if (value == "Jungle Cruise") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9e9e6a3c12db4ad1e79e', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9e9e6a3c12db4ad1e79e', '_self');
 
       } else if (value == "The Suicide Squad") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9fbb6a3c12db4ad1e79f', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9fbb6a3c12db4ad1e79f', '_self');
 
       } else if (value == "Snake Eyes") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617a9ffd6a3c12db4ad1e7a0', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617a9ffd6a3c12db4ad1e7a0', '_self');
 
       } else if (value == "Shang-Chi and the Legend of the Ten Rings") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617aa04c6a3c12db4ad1e7a1', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617aa04c6a3c12db4ad1e7a1', '_self');
 
       } else if (value == "Army of the Dead") {
-        open('https://8080-black-tiglon-4tn2fyxj.ws-eu18.gitpod.io/view_films/617ab297bd56ec8e94cb8361', '_self');
+        open('http://director-dad.herokuapp.com/view_films/617ab297bd56ec8e94cb8361', '_self');
 
       }
     }
